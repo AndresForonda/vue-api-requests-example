729c9f4149211b4e8f1eedb47ac842d3bd2a1a11
Andres Foronda <andresforonda.af@gmail.com>
2021-07-13 10:24:13 -0400

    Add BeerCard component for Beer request examples

---
+++ src/components/BeerCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-96 bg-blue-900 rounded-xl p-10">
+    <img
+      :src="
+        beer.image_url
+          ? beer.image_url
+          : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'
+      "
+      alt="beer_img"
+      class="h-80 mx-auto mb-5"
+    />
+
+    <div class="text-white">
+      <p class="font-bold text-2xl">{{ beer.name }}</p>
+      <p class="font-thin text-xl italic mb-3">{{ beer.tagline }}</p>
+      <p>{{ beer.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({ beer: { type: Object, required: true } });
+</script>
+
+<style></style>
